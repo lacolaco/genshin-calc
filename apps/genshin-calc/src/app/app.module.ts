d27bdf2e9c8807b530502b7136ca51da5ace9f19
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-01-23 13:57:25 +0900

    refactor: use AngularFire

--- apps/genshin-calc/src/app/app.module.ts
+++ apps/genshin-calc/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirePerformanceModule } from '@angular/fire/performance';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AlbedoTransientBlossomsModule } from './features/albedo-transient-brossoms/module';
 import { ZhongliPlanetBefallModule } from './features/zhongli-planet-befall/module';
 import { GanyuLiutianArcheryModule } from './features/ganyu-liutian-archery/module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  // eslint-disable-next-line max-len
-  imports: [BrowserModule, AppRoutingModule, CoreModule, ZhongliPlanetBefallModule, AlbedoTransientBlossomsModule, GanyuLiutianArcheryModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAnalyticsModule,
+    AngularFirePerformanceModule,
+    CoreModule,
+    ZhongliPlanetBefallModule,
+    AlbedoTransientBlossomsModule,
+    GanyuLiutianArcheryModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/genshin-calc/src/environments/environment.ts
+++ apps/genshin-calc/src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAs_sMoZFlvvdDjNlI9RSCSNnxzlM8BJb8',
+    authDomain: 'genshin-calc-446b5.firebaseapp.com',
+    projectId: 'genshin-calc-446b5',
+    storageBucket: 'genshin-calc-446b5.appspot.com',
+    messagingSenderId: '970449511834',
+    appId: '1:970449511834:web:46eef640b68f1f79518623',
+    measurementId: 'G-H55GPK3WXT',
+  },
 };
 
 /*
