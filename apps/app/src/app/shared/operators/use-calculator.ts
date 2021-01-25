a01a4bef0b0261117b1aa63d0d99d694eb41aec0
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-01-25 09:16:39 +0900

    refactor: simplify type annotation

--- apps/app/src/app/shared/operators/use-calculator.ts
+++ apps/app/src/app/shared/operators/use-calculator.ts
@@ -1,13 +1,15 @@
 import { Calculation, Calculator } from '@genshin-calc/core';
 import { patch } from '@rx-angular/state';
-import { Observable, pipe } from 'rxjs';
+import { pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export const useCalculator = <T extends { calculatorParams: Params }, Params = unknown>(
   calculator: Calculator<Params>,
 ) => {
-  return pipe<Observable<T>, Observable<T & { calculation: Calculation }>>(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    map((value) => patch(value as any, { calculation: calculator(value.calculatorParams) })),
+  return pipe(
+    map<T, T & { calculation: Calculation }>((value) =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      patch(value as any, { calculation: calculator(value.calculatorParams) }),
+    ),
   );
 };
