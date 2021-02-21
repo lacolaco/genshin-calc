3f71961dc61ce59df6e348cb825d3b234f8acac1
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-02-21 18:31:54 +0900

    chore: implement core-calculator generator

---
+++ tools/generators/core-calculator/index.ts
@@ -0,0 +1,13 @@
+import { formatFiles, generateFiles, readProjectConfiguration, Tree } from '@nrwl/devkit';
+import * as path from 'path';
+
+export default async function (host: Tree, schema: { name: string }) {
+  const coreProject = readProjectConfiguration(host, 'core');
+
+  await generateFiles(host, path.resolve(__dirname, './files'), path.resolve(coreProject.root, 'src/lib/calculator'), {
+    name: schema.name,
+    tmpl: '',
+  });
+  await formatFiles(host);
+  return () => {};
+}
